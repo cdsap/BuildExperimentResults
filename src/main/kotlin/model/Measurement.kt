079ba01aa323cba0dca7aba2123be2685e81380c
Inaki Villar <inaki.seri@gmail.com>
2023-04-26 15:21:18 -0700

    initial import

---
+++ src/main/kotlin/model/Measurement.kt
@@ -0,0 +1,51 @@
+package model
+
+import io.github.cdsap.geapi.client.model.OS
+
+class Measurement(
+    val category: String, val name: String, val variantA: Any, val variantB: Any, val OS: OS
+) {
+    fun diff(): Any {
+        if (variantA is Int) {
+            if ((variantA as Int) - (variantB as Int) != 0) {
+                if (variantA != 0) {
+                    val x = (variantB * 100) / variantA
+                    val result = 100 - x
+                    return "$result%"
+                } else {
+                    return ""
+                }
+            } else {
+                return ""
+            }
+
+        } else if (variantA is Long) {
+            if ((variantA as Long) - (variantB as Long) != 0L) {
+                if (variantA != 0L) {
+                    val x = (variantB * 100L) / variantA
+                    val result = 100L - x
+                    return "$result%"
+                } else {
+                    return ""
+                }
+            } else {
+                return ""
+            }
+
+        } else if (variantA is Double) {
+            if ((variantA as Double) - (variantB as Double) != 0.0) {
+                val x = (variantB * 100.0) / variantA
+                val result = 100 - x
+                return "$result%"
+            } else {
+                return ""
+            }
+        } else {
+
+        }
+        return ""
+    }
+
+}
+
+data class TaskFingerprintingSummary(val count: Int, val serialDuration: Long)
---
+++ src/main/kotlin/report/ExperimentReport.kt
@@ -0,0 +1,147 @@
+package report
+
+import io.github.cdsap.geapi.client.domain.impl.GetBuildScansWithQueryImpl
+import io.github.cdsap.geapi.client.model.*
+import io.github.cdsap.geapi.client.repository.impl.GradleRepositoryImpl
+import io.github.cdsap.geapi.domain.model.Task
+import model.Measurement
+import view.ExperimentView
+
+
+class ExperimentReport(
+    private val filter: Filter,
+    private val repository: GradleRepositoryImpl
+) {
+
+    suspend fun process() {
+        val getBuildScans = GetBuildScansWithQueryImpl(repository).get(filter)
+        val buildsFiltered = mutableListOf<Build>()
+        if (filter.variants == null) {
+            throw IllegalArgumentException("Variants can not be null")
+        } else {
+            val variants = filter.variants!!.split(",")
+            val variantA = variants[0].trim()
+            val variantB = variants[1].trim()
+            if (getBuildScans.isNotEmpty()) {
+                println("Processing build scan cache performance")
+                getBuildScans.map {
+                    if (filter.experimentId != null) {
+                        if (it.tags.contains(filter.experimentId) && it.tags.contains(variantB)) {
+                            collectBuild(it, buildsFiltered, Experiment.VARIANT_B)
+                        } else if (it.tags.contains(filter.experimentId) && it.tags.contains(variantA)) {
+                            collectBuild(it, buildsFiltered, Experiment.VARIANT_A)
+                        } else {
+
+                        }
+                    } else {
+                        if (it.tags.contains("experiment") && it.tags.contains(variantB)) {
+                            collectBuild(it, buildsFiltered, Experiment.VARIANT_B)
+                        } else if (it.tags.contains("experiment") && it.tags.contains(variantA)) {
+                            collectBuild(it, buildsFiltered, Experiment.VARIANT_A)
+                        }
+                    }
+                }
+            }
+            ExperimentView().print(get(buildsFiltered))
+        }
+    }
+
+    private suspend fun collectBuild(
+        it: ScanWithAttributes,
+        builds: MutableList<Build>,
+        experiment: Experiment
+    ) {
+        var os = if (it.tags.contains("Mac OS X")) {
+            OS.MAC
+        } else if (it.tags.contains("Linux")) {
+            OS.Linux
+        } else {
+            null
+        }
+        if (os != null) {
+            val cachePerformance = repository.getBuildScanGradleCachePerformance(it.id)
+            cachePerformance.experiment = experiment
+            cachePerformance.id = it.id
+            cachePerformance.requestedTask = it.requestedTasksGoals
+            cachePerformance.tags = it.tags
+            cachePerformance.buildDuration = it.buildDuration
+            cachePerformance.OS = os
+            builds.add(cachePerformance)
+        }
+    }
+
+    fun get(builds: List<Build>): List<Measurement> {
+        return builds.groupBy { it.OS }.flatMap {
+            javaMeasurements(
+                it.value.filter { it.experiment == Experiment.VARIANT_A }.dropLast(2),
+                it.value.filter { it.experiment == Experiment.VARIANT_B }.dropLast(2),
+                it.key
+            )
+        }
+    }
+
+    private fun javaMeasurements(
+        variantABuilds: List<Build>,
+        variantBBuilds: List<Build>,
+        os: OS
+    ): List<Measurement> {
+
+        val taskTypes = variantABuilds[0].taskExecution.filter {
+            (it.avoidanceOutcome == "executed_cacheable" || it.avoidanceOutcome == "executed_not_cacheable")
+        }.distinctBy { it.taskType }
+        val measurements = mutableListOf<Measurement>()
+        taskTypes.forEach { task ->
+
+            val sumVariantA = variantABuilds.sumOf {
+                filterByExecutionAndType(it, task)
+                    .sumOf { it.duration }
+            } / variantABuilds.sumOf {
+                filterByExecutionAndType(it, task)
+                    .count()
+            }
+            var process = true
+
+            if (sumVariantA < 100L) {
+                process = false
+            }
+
+            if (process) {
+                measurements.add(
+                    Measurement(
+                        category = "Tasks Compiler",
+                        name = task.taskType,
+                        variantA = variantABuilds.sumOf {
+                            filterByExecutionAndType(it, task)
+                                .sumOf { it.duration }
+                        } /
+                            variantABuilds.sumOf {
+                                filterByExecutionAndType(it, task)
+                                    .count()
+                            },
+                        variantB = variantBBuilds.sumOf {
+                            filterByExecutionAndType(it, task)
+                                .sumOf { it.duration }
+                        } /
+                            variantBBuilds.sumOf {
+                                filterByExecutionAndType(it, task)
+                                    .count()
+                            },
+                        OS = os
+                    )
+                )
+            }
+        }
+        measurements.sortBy { it.name }
+        return measurements
+
+    }
+
+    private fun filterByExecutionAndType(
+        it: Build,
+        task: Task
+    ) = it.taskExecution.filter {
+        (it.avoidanceOutcome == "executed_cacheable" || it.avoidanceOutcome == "executed_not_cacheable")
+            && it.taskType == task.taskType
+    }
+
+}
