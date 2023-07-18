15e1fa26424a8171689d52a5dff824fe0b2312a1
Inaki Villar <inaki.seri@gmail.com>
2023-07-18 15:14:37 -0700

    adding kotlin build reports

--- src/main/kotlin/io/github/cdsap/compare/report/ExperimentReport.kt
+++ src/main/kotlin/io/github/cdsap/compare/report/ExperimentReport.kt
@@ -33,8 +33,10 @@ class ExperimentReport(
                 }
             }
 
-
-            ExperimentView().print(get(outcome), variants[0], variants[1])
+            val measurements = get(outcome)
+            if (measurements.isNotEmpty()) {
+                ExperimentView().print(get(outcome), variants[0], variants[1])
+            }
         }
     }
 
@@ -51,6 +53,12 @@ class ExperimentReport(
             kotlinProcessMeasurement(
                 it.value.filter { it.experiment == Experiment.VARIANT_A }.first(),
                 it.value.filter { it.experiment == Experiment.VARIANT_B }.first(),
+                it.key
+            )
+        } + builds.groupBy { it.OS }.flatMap {
+            kotlinBuildReportMeasurement(
+                it.value.filter { it.experiment == Experiment.VARIANT_A }.dropLast(2),
+                it.value.filter { it.experiment == Experiment.VARIANT_B }.dropLast(2),
                 it.key
             )
         }
@@ -151,6 +159,144 @@ class ExperimentReport(
 
     }
 
+    private fun kotlinBuildReportMeasurement(
+        variantABuilds: List<Build>,
+        variantBBuilds: List<Build>, key: OS
+    ): List<Measurement> {
+        val measurements = mutableListOf<Measurement>()
+        val valuesVariantA = extracted(variantABuilds)
+        val valuesVariantB = extracted(variantBBuilds)
+
+        val valuesByTaskAggregated = aggregateBuilds(valuesVariantA)
+        val valuesByTaskAggregatedB = aggregateBuilds(valuesVariantB)
+
+
+        valuesByTaskAggregated.forEach {
+            val x = valuesByTaskAggregatedB[it.key]
+            if (x != null && it.value.size == x.size) {
+                if (it.value.filter {
+                        it.contains("ms") || it.contains("GB") || it.contains("MB") || it.contains("KB") || it.contains(
+                            "B"
+                        )
+                    }.isNotEmpty()) {
+
+                    val valuesFormattedA = it.value.map { it.replace(",", "").replace("ms", "").split(" ")[0] }
+                    val valuesFormattedB = x.map { it.replace(",", "").replace("ms", "").split(" ")[0] }
+                    val qualifierA = if (it.value.first().contains("ms")) "ms" else it.value.first().split(" ")[1]
+                    val qualifierB = if (x.first().contains("ms")) "ms" else x.first().split(" ")[1]
+
+
+                    val varianta = valuesFormattedA.sumOf { it.toDouble() } / valuesFormattedA.size
+                    val variantb = valuesFormattedB.sumOf { it.toDouble() } / valuesFormattedB.size
+                    if (varianta != variantb) {
+                        measurements.add(
+                            Measurement(
+                                name = it.key,
+                                variantA = "$varianta $qualifierA",
+                                variantB = "$variantb $qualifierB",
+                                category = "Kotlin Build Reports",
+                                OS = OS.Linux
+                            )
+                        )
+                    }
+                } else {
+                    val valuesFormattedA = it.value.map { it.replace(",", "").split(" ")[0] }
+                    val valuesFormattedB = x.map { it.replace(",", "").split(" ")[0] }
+                    val varianta = valuesFormattedA.sumOf { it.toLong() } / valuesFormattedA.size
+                    val variantb = valuesFormattedB.sumOf { it.toLong() } / valuesFormattedB.size
+                    if (varianta != variantb) {
+                        measurements.add(
+                            Measurement(
+                                name = it.key,
+                                variantA = "$varianta",
+                                variantB = "$variantb",
+                                category = "Kotlin Build Reports",
+                                OS = OS.Linux
+                            )
+                        )
+                    }
+                }
+
+            }
+
+        }
+        return measurements
+
+
+    }
+
+    private fun aggregateBuilds(builds: Map<String, Map<String, MutableList<MetricKotlin>>>): MutableMap<String, MutableList<String>> {
+        val valuesByTaskAggregated = mutableMapOf<String, MutableList<String>>()
+        builds.forEach {
+            it.value.forEach {
+                it.value.forEach {
+                    if (!valuesByTaskAggregated.containsKey("${it.desc}")) {
+                        valuesByTaskAggregated["${it.desc}"] = mutableListOf()
+                    }
+
+                    valuesByTaskAggregated["${it.desc}"]?.add(it.value)
+                }
+            }
+
+        }
+        return valuesByTaskAggregated
+    }
+
+    private fun extracted(
+        buildsPerVariant: List<Build>
+    ): MutableMap<String, Map<String, MutableList<MetricKotlin>>> {
+        val variantBuild = mutableMapOf<String, Map<String, MutableList<MetricKotlin>>>()
+        buildsPerVariant.forEach {
+            val buildId = it.id
+            val buildsWithKotlinBuildReports =
+                it.values.filter { it.name.contains("Kotlin") && it.value.contains("Non incremental build because") }
+            val tasksWithMetrics = mutableMapOf<String, MutableList<MetricKotlin>>()
+            if (buildsWithKotlinBuildReports.isNotEmpty()) {
+                buildsWithKotlinBuildReports.forEach {
+                    val key = it.name
+                    val values = it.value.split("Performance: [")[1].split("]")[0].split(": ")
+                    var auxCount = 0
+                    while (auxCount < values.size - 1) {
+                        var key2 = ""
+                        var value = ""
+                        if (auxCount == 0) {
+                            key2 = values[auxCount]
+                        } else {
+                            key2 = values[auxCount].split(",").last()
+                        }
+
+                        if (values[auxCount + 1].split(",").count() > 2) {
+
+                            // we found issues parsing lines like
+                            // 1},2023-07-18T01:17:40,Number of times classpath snapshot is loaded
+                            // to avoid that, we need to identify if the "," comes from a byte
+                            if ((values[auxCount + 1].contains("GB") || values[auxCount + 1].contains("MB") || values[auxCount + 1].contains(
+                                    "KB"
+                                ) || values[auxCount + 1].contains("B"))
+                            ) {
+                                value = values[auxCount + 1].split(",").dropLast(1).joinToString { "" }.replace("}", "")
+                            } else {
+                                value = values[auxCount + 1].split(",")[0].replace("}", "")
+                            }
+
+                        } else {
+                            value = values[auxCount + 1].split(",")[0].replace("}", "")
+                        }
+                        if (!tasksWithMetrics.containsKey(key)) {
+                            tasksWithMetrics[key] = mutableListOf()
+                        }
+                        tasksWithMetrics[key]?.add(MetricKotlin(key2, value))
+                        auxCount += 1
+
+                    }
+                }
+                variantBuild[buildId] = tasksWithMetrics
+            }
+        }
+        return variantBuild
+    }
+
+
     private fun filterByExecutionAndType(
         it: Build,
         task: Task
@@ -160,3 +306,6 @@ class ExperimentReport(
     }
 
 }
+
+
+data class MetricKotlin(val desc: String, val value: String)
